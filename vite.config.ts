0db6f5acc036e4e6bf4875b8d9729c76dba5e4a2
Lenhard Pedro Malana <malana.lenhard.02152003@gmail.com>
2026-06-02 17:49:03 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tsconfigPaths from "vite-tsconfig-paths";
-import { cloudflare } from "@cloudflare/vite-plugin";
-
 export default defineConfig({
   plugins: [
-    react(),
-    tsconfigPaths(),
-    cloudflare() as any
+    // your plugins like TanStackRouterVite(), react()
   ],
-  server: {
-    host: "::",
-    port: 8080,
+  ssr: {
+    noExternal: ['@tanstack/start', '@tanstack/start-server-core'],
   },
-  build: {
-    chunkSizeWarningLimit: 3000,
-    minify: "esbuild",
-    cssMinify: true,
-    target: "esnext",
-    outDir: "dist"
+  optimizeDeps: {
+    include: ['@tanstack/start', '@tanstack/start-server-core'],
   }
 });
