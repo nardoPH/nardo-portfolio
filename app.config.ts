20898492a9c31aa827cecfa7a74d4a7c689e986c
Lenhard Pedro Malana <malana.lenhard.02152003@gmail.com>
2026-06-02 23:10:17 +0800

    Update app.config.ts

--- app.config.ts
+++ app.config.ts
@@ -1,1 +1,13 @@
 import { defineConfig } from '@tanstack/react-start/config';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+  server: {
+    preset: 'cloudflare-pages'
+  },
+  vite: {
+    plugins: [
+      tsconfigPaths(),
+    ],
+  },
+});
