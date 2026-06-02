cb37369cabc780e72480c8905489a75ffdb544b9
Lenhard Pedro Malana <malana.lenhard.02152003@gmail.com>
2026-06-02 18:00:43 +0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,13 @@
-import { defineConfig } from "vite";
-// @ts-ignore
-import lovableConfig from "@lovable.dev/vite-tanstack-config";
+import { defineConfig } from 'vite';
+import { tanstackBuildConfig } from '@lovable.dev/vite-tanstack-config';
 
-export default defineConfig(lovableConfig);
+export default defineConfig({
+  ...tanstackBuildConfig,
+  // This explicitly prevents Vite from searching for a missing index.html file
+  build: {
+    ...tanstackBuildConfig?.build,
+    rollupOptions: {
+      ...tanstackBuildConfig?.build?.rollupOptions,
+    }
+  }
+});
